9273f58befb24c73df1e7646146634364ef5e7fa
Anthony Baker <anthony@deluxeluxury.com>
2015-05-07 23:01:02 -0400

    Updating Config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+//environment config:
+$env_config = array(
+	'db_host' => "127.0.0.1", //or localhost
+	'db_user' => "DATABASE-USER",
+	'db_pass' => "DATABASE-PASSWORD",
+	'db_name' => "DATABASE-NAME",
+	'db_port' => "3306"
+);
+
+?>
